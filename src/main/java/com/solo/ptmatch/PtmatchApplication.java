cb5b9e17facbf62980daf3a000216fcd65dc4bc1
tojaman <zzoon5020@naver.com>
2025-09-15 20:29:07 +0900

    first commit

---
+++ src/main/java/com/solo/ptmatch/PtmatchApplication.java
@@ -0,0 +1,13 @@
+package com.solo.ptmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtmatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solo/ptmatch/PtmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solo.ptmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
