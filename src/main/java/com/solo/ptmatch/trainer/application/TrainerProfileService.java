7ea40129511dccaeafd31abf4969aa6b17c7c716
Tojaman <zzoon5020@naver.com>
2025-09-30 23:55:38 +0900

    feat(trainer) #4 Trainer 리스트 조회 기능 구현

--- src/main/java/com/solo/ptmatch/trainer/application/TrainerProfileService.java
+++ src/main/java/com/solo/ptmatch/trainer/application/TrainerProfileService.java
@@ -1,5 +1,8 @@
 package com.solo.ptmatch.trainer.application;
 
+import com.solo.ptmatch.trainer.domain.Specialty;
+import com.solo.ptmatch.trainer.domain.TrainerProfile;
+import com.solo.ptmatch.trainer.infrastructure.TrainerProfileRepository;
 import com.solo.ptmatch.trainer.presentation.request.TrainerProfileUpsertRequest;
 import com.solo.ptmatch.trainer.presentation.request.TrainerSearchRequest;
 import com.solo.ptmatch.trainer.presentation.response.TrainerDetailResponse;
@@ -8,14 +11,33 @@ import com.solo.ptmatch.trainer.presentation.response.TrainerSummaryResponse;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class TrainerProfileService {
 
-    public List<TrainerSummaryResponse> getTrainerSummaries(TrainerSearchRequest trainerSearchRequest) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    private final TrainerProfileRepository trainerProfileRepository;
+
+    public List<TrainerSummaryResponse> getTrainerSummaries(TrainerSearchRequest request) {
+        Sort sort = createSort(request.sort());
+        Pageable pageable = PageRequest.of(request.page(), request.size(), sort);
+
+        Specialty specialty = request.specialty() != null ? Specialty.valueOf(request.specialty().toUpperCase()) : null;
+
+        Page<TrainerProfile> trainerPage = trainerProfileRepository.searchBySpecialtyAndGymAddress(
+                specialty,
+                request.region(),
+                pageable
+        );
+
+        return trainerPage.getContent().stream()
+                .map(TrainerSummaryResponse::from)
+                .toList();
     }
 
     public TrainerDetailResponse getTrainerDetail(Long trainerId) {
@@ -28,5 +50,16 @@ public class TrainerProfileService {
 
     public TrainerProfileUpsertResponse updateTrainerProfile(TrainerProfileUpsertRequest trainerProfileRegisterRequest) {
         throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    private Sort createSort(String sortString) {
+        if (sortString == null || sortString.isBlank()) {
+            return Sort.unsorted();
+        }
+
+        String[] parts = sortString.split("_");
+        String property = parts[0];
+        Sort.Direction direction = Sort.Direction.fromString(parts[1]); // 정렬(오름/내림차순)
+        return Sort.by(direction, property);
     }
 }
