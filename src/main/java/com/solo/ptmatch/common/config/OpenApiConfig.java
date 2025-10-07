30ec3b904285a204a8bba1784056c2ed04df8566
Tojaman <zzoon5020@naver.com>
2025-10-07 11:30:17 +0900

    feat swagger Header에 JWT 토큰 추가 기능

--- src/main/java/com/solo/ptmatch/common/config/OpenApiConfig.java
+++ src/main/java/com/solo/ptmatch/common/config/OpenApiConfig.java
@@ -1,9 +1,42 @@
 package com.solo.ptmatch.common.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
-    // TODO: configure OpenAPI documentation
+    @Bean
+    public OpenAPI openAPI() {
+        // API 정보 정의
+        Info info = new Info()
+                .title("PT Match API")
+                .version("v1.0.0")
+                .description("PT Match 프로젝트 API 명세서입니다.");
+
+        // Security Scheme 이름 정의
+        String securitySchemeName = "bearerAuth";
+
+        // Security Requirement 정의
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securitySchemeName);
+
+        // Security Scheme 정의
+        Components components = new Components()
+                .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        // OpenAPI 객체 생성 및 설정
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
 }
