af51f5b719135cd70dfc198c28cb0b69a5b58d99
Tojaman <zzoon5020@naver.com>
2025-12-06 21:59:24 +0900

    매칭 스케줄 응답 DTO 수정
    - 예약 가능 여부 필터링을 위한 reservationStatus 필드 추가

--- src/main/java/com/solo/ptmatch/trainer/presentation/response/TrainerScheduleListResponse.java
+++ src/main/java/com/solo/ptmatch/trainer/presentation/response/TrainerScheduleListResponse.java
@@ -1,33 +1,34 @@
 package com.solo.ptmatch.trainer.presentation.response;
 
 import com.solo.ptmatch.trainer.domain.AvailableSchedule;
+import com.solo.ptmatch.trainer.domain.ReservationStatus;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public record TrainerScheduleListResponse(
 
-        Long scheduleId,
+                Long scheduleId,
 
-        @NotNull
-        LocalDateTime startTime,
+                @NotNull LocalDateTime startTime,
 
-        @NotNull
-        LocalDateTime endTime
-) {
-        public static TrainerScheduleListResponse of(Long scheduleId, LocalDateTime startTime, LocalDateTime endTime) {
+                @NotNull @NotNull LocalDateTime endTime,
+
+                ReservationStatus reservationStatus) {
+        public static TrainerScheduleListResponse of(Long scheduleId, LocalDateTime startTime, LocalDateTime endTime,
+                        ReservationStatus reservationStatus) {
                 return new TrainerScheduleListResponse(
-                        scheduleId,
-                        startTime,
-                        endTime
-                );
+                                scheduleId,
+                                startTime,
+                                endTime,
+                                reservationStatus);
         }
 
         public static TrainerScheduleListResponse from(AvailableSchedule availableSchedule) {
                 return new TrainerScheduleListResponse(
-                        availableSchedule.getId(),
-                        availableSchedule.getStartTime(),
-                        availableSchedule.getEndTime()
-                );
+                                availableSchedule.getId(),
+                                availableSchedule.getStartTime(),
+                                availableSchedule.getEndTime(),
+                                availableSchedule.getReservationStatus());
         }
 }
