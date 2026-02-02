3f2fceb82d5b710a2234c7f8ea411d8dc9e0c207
Tojaman <zzoon5020@naver.com>
2026-02-03 04:57:36 +0900

    feat #72 캐시 의존성 주입 및 캐시 설정(config)코드 작성

---
+++ src/main/java/com/solo/ptmatch/common/config/CacheConfig.java
@@ -0,0 +1,57 @@
+package com.solo.ptmatch.common.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.solo.ptmatch.common.cache.enums.CacheType;
+import com.solo.ptmatch.common.cache.enums.CacheDef;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import com.github.benmanes.caffeine.cache.Cache;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+    // 로컬(Caffeine) 캐시 매니저에 캐시 생성
+    @Bean
+    @Primary
+    public CacheManager caffeineCacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        List<CaffeineCache> caches = Arrays.stream(CacheDef.values())
+                .filter(def -> def.getType() != CacheType.GLOBAL) // LOCAL, LAYERED 캐시만 생성
+                .map(def -> new CaffeineCache(def.getName(), buildLocalCache(def)))
+                .toList();
+        cacheManager.setCaches(caches); // 캐시 매니저에 주입
+        return cacheManager;
+    }
+
+    private Cache<Object, Object> buildLocalCache(CacheDef def) {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(Duration.ofSeconds(def.getTtlSeconds())).build();
+    }
+
+    // @Bean
+    // public RedisTemplate<String, Object> cacheRedisTemplate(
+    //         RedisConnectionFactory connectionFactory,
+    //         ObjectMapper objectMapper) {
+    //     RedisTemplate<String, Object> template = new RedisTemplate<>();
+    //     template.setConnectionFactory(connectionFactory);
+
+    //     StringRedisSerializer keySerializer = new StringRedisSerializer();
+    //     GenericJackson2JsonRedisSerializer valueSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+    //     template.setKeySerializer(keySerializer);
+    //     template.setHashKeySerializer(keySerializer);
+    //     template.setValueSerializer(valueSerializer);
+    //     template.setHashValueSerializer(valueSerializer);
+    //     template.afterPropertiesSet();
+    //     return template;
+    // }
+}
