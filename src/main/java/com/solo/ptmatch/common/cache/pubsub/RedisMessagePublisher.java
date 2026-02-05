e94db240d6ca35b7da64995c4183fe6a2b179b7b
Tojaman <zzoon5020@naver.com>
2026-02-06 05:05:51 +0900

    feat #72 Redis Pub 예외 전파 방지 - 문제: pub 예외가 트랜잭션에 전파되면 트랜잭션 실패하여 실패 응답 발행 - ✅상황1: 조회용 데이터 특성상 일정 기간 데이터 불일치 허용 (요청 처리가 더 중요) - ❌상황2: 동기화가 정말 중요하다면 outbox + MQ/재시도 로직으로 해결 가능(현재 사용X) - 결론: 예외 전파를 방지하여 성공 응답

--- src/main/java/com/solo/ptmatch/common/cache/pubsub/RedisMessagePublisher.java
+++ src/main/java/com/solo/ptmatch/common/cache/pubsub/RedisMessagePublisher.java
@@ -1,7 +1,6 @@
 package com.solo.ptmatch.common.cache.pubsub;
 
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
@@ -19,11 +18,14 @@ public class RedisMessagePublisher {
 
     private final ObjectMapper objectMapper;
 
-    @SneakyThrows
     public void publish(CacheTarget cacheTarget, String key) {
-        CacheEvictionMessage msg = new CacheEvictionMessage(cacheTarget, key);
-        String message = objectMapper.writeValueAsString(msg);
-        redisTemplate.convertAndSend(topic.getTopic(), message);
-        log.info("publish 완료");
+        try {
+            CacheEvictionMessage msg = new CacheEvictionMessage(cacheTarget, key);
+            String message = objectMapper.writeValueAsString(msg);
+            redisTemplate.convertAndSend(topic.getTopic(), message);
+            log.info("publish 완료");
+        } catch (Exception e) {
+            log.warn("Redis Pub/Sub publish 실패: cacheTarget={}, key={}", cacheTarget, key, e);
+        }
     }
 }
