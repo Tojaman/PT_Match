7afc589113659282044e0b73e548cfc09bab2233
Tojaman <zzoon5020@naver.com>
2026-02-25 07:28:35 +0900

    chore: 기타 문법 오류 수정

--- src/main/java/com/solo/ptmatch/matching/infrastructure/AvailableScheduleRepository.java
+++ src/main/java/com/solo/ptmatch/matching/infrastructure/AvailableScheduleRepository.java
@@ -5,6 +5,8 @@ import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -18,7 +20,7 @@ public interface AvailableScheduleRepository extends JpaRepository<AvailableSche
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT s FROM AvailableSchedule s WHERE s.id IN :ids")
-    public List<AvailableSchedule> findAllByIdInWithLock(List<Long> ids);
+    public List<AvailableSchedule> findAllByIdInWithLock(@Param("ids") List<Long> ids);
 
     @Query("SELECT COUNT(s) FROM AvailableSchedule s " +
             "WHERE s.trainerProfile.id = :trainerId " +
